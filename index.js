9f77f99af60ee02c2813ab0222a43545c097458d
KevalKThumar <thumarkeval12@gmail.com>
2023-09-02 13:45:22 +0530

    commit

--- index.js
+++ index.js
@@ -8,28 +8,36 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const PORT = process.env.PORT || 5000;
-const MOGODB_url =
-  "mongodb+srv://keval:keval@flutternotes.nkafc0p.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose
-  .connect(MOGODB_url)
-  .then(function () {
-
-    app.get("/", (req, res) => {
-      res.send(`<h1>Hello World</h1>`);
-    });
-    app.get('/user', (req, res) => {
-      res.send({
-         name: "keval",
-         age: "19"
-       })
-    })
-    // app.use("/notes", noteRouter)
-
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
+app.get('/user', (req, res) => {
+  res.send({
+    name: "keval",
+    age: "19"
   })
-  .catch(() => {
-    throw `Error while Connecting MongoDB`;
-  });
+})
+// const MOGODB_url =
+//   "mongodb+srv://keval:keval@flutternotes.nkafc0p.mongodb.net/?retryWrites=true&w=majority";
+
+// mongoose
+//   .connect(MOGODB_url)
+//   .then(function () {
+
+//     app.get("/", (req, res) => {
+//       res.send(`<h1>Hello World</h1>`);
+//     });
+//     app.get('/user', (req, res) => {
+//       res.send({
+//          name: "keval",
+//          age: "19"
+//        })
+//     })
+//     // app.use("/notes", noteRouter)
+//   })
+//   .catch(() => {
+//     throw `Error while Connecting MongoDB`;
+//   });
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
