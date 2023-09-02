61af39e6c3b1e6ebaad6a97a119c8782cd986c84
KevalKThumar <thumarkeval12@gmail.com>
2023-09-02 13:17:07 +0530

    commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 const port = 9000;
-app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
 });
 
 app.listen(9000, () => {
