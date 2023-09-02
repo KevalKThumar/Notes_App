98e69936cff10d5fc48ab7eae308ccab5b70e268
KevalKThumar <thumarkeval12@gmail.com>
2023-09-02 13:56:53 +0530

    commit

--- index.js
+++ index.js
@@ -8,36 +8,28 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const PORT = process.env.PORT || 5000;
+
+const MOGODB_url =
+  "mongodb+srv://keval:keval@flutternotes.nkafc0p.mongodb.net/?retryWrites=true&w=majority";
+
+await mongoose.connect(MOGODB_url).then(() => {
+  console.log(`DB connected`)
+}).catch(() => {
+  throw `Error while Connecting MongoDB`;
+});
+
+
 app.get("/", (req, res) => {
   res.send(`<h1>Hello World</h1>`);
 });
+
 app.get('/user', (req, res) => {
   res.send({
     name: "keval",
     age: "19"
   })
 })
-// const MOGODB_url =
-//   "mongodb+srv://keval:keval@flutternotes.nkafc0p.mongodb.net/?retryWrites=true&w=majority";
 
-// mongoose
-//   .connect(MOGODB_url)
-//   .then(function () {
-
-//     app.get("/", (req, res) => {
-//       res.send(`<h1>Hello World</h1>`);
-//     });
-//     app.get('/user', (req, res) => {
-//       res.send({
-//          name: "keval",
-//          age: "19"
-//        })
-//     })
-//     // app.use("/notes", noteRouter)
-//   })
-//   .catch(() => {
-//     throw `Error while Connecting MongoDB`;
-//   });
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
