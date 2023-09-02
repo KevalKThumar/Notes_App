de342b126fbf920976b74bf5405dba0468b5d7d1
KevalKThumar <thumarkeval12@gmail.com>
2023-09-02 13:42:52 +0530

    commit

--- index.js
+++ index.js
@@ -6,18 +6,31 @@ const app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
-const port = 9000;
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello World</h1>`);
-});
 
-app.get('/home', (req, res) => {
-  res.send({
-    "name": "keval",
-    "age": 19
+const PORT = process.env.PORT || 5000;
+const MOGODB_url =
+  "mongodb+srv://keval:keval@flutternotes.nkafc0p.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MOGODB_url)
+  .then(function () {
+
+    app.get("/", (req, res) => {
+      res.send(`<h1>Hello World</h1>`);
+    });
+    app.get('/user', (req, res) => {
+      res.send({
+         name: "keval",
+         age: "19"
+       })
+    })
+    // app.use("/notes", noteRouter)
+
   })
-})
+  .catch(() => {
+    throw `Error while Connecting MongoDB`;
+  });
 
-app.listen(9000, () => {
-  console.log(`Starting Server on Port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
