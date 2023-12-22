d947d6793e798f59fbab8d2ad422483b74015164
KevalKThumar <thumarkeval12@gmail.com>
2023-12-22 20:15:26 +0530

    .

--- middelwares/auth.js
+++ middelwares/auth.js
@@ -6,12 +6,12 @@ const auth = async (req, res, next) => {
         if (!token) {
             return res.status(401).json({ message: "No auth token , access denide" });
         }
-        const isVerified = JWT.verify(token, "13QEWDSFGrty345*&yFs!@tgji*&GFD67^hgGFFFH");
-        if (!isVerified) {
-            res.status(401).json({ message: "Token verification failed,authorization denied." });
-        }
-        req.user = isVerified._id;
-        req.token = token;
+        // const isVerified = JWT.verify(token, "13QEWDSFGrty345*&yFs!@tgji*&GFD67^hgGFFFH");
+        // if (!isVerified) {
+        //     res.status(401).json({ message: "Token verification failed,authorization denied." });
+        // }
+        // req.user = isVerified._id;
+        // req.token = token;
         // next is use for add next call back function in api call.
         next();
     }
